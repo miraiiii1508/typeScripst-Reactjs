c1e5fcc014f499793b1f397251d5be42689e2ca8
evondev <trananhtuan400@gmail.com>
2024-03-19 00:08:51 +0700

    add more exercises

---
+++ main/src/problems/generic/04.generic.tsx
@@ -0,0 +1,48 @@
+import { Equal, Expect } from "../../helpers/type-utils";
+
+interface Button {
+  value: string;
+  label: string;
+}
+
+interface ButtonGroupProps {
+  buttons: Button[];
+  onClick: (value: string) => void;
+}
+
+const ButtonGroup = (props: ButtonGroupProps) => {
+  return (
+    <div>
+      {props.buttons.map((button) => {
+        return (
+          <button
+            key={button.value}
+            onClick={() => {
+              props.onClick(button.value);
+            }}
+          >
+            {button.label}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+<>
+  <ButtonGroup
+    onClick={(value) => {
+      type test = Expect<Equal<typeof value, "add" | "delete">>;
+    }}
+    buttons={[
+      {
+        value: "add",
+        label: "Add",
+      },
+      {
+        value: "delete",
+        label: "Delete",
+      },
+    ]}
+  ></ButtonGroup>
+</>;
